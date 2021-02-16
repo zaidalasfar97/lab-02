5c84ac04571496601e1ead9d162fad9f8a3e0243
zaidalasfar97 <z.alasfar@hotmail.com>
2021-02-16 02:59:41 -0800

    done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 'use strict';
 let mainObj = document.getElementById('firstMain');
 let arrObj = [];
-
 $(function () {
     function Image(value) {
         this.image_url = value.image_url;
@@ -15,8 +14,8 @@ $(function () {
         data.forEach((item) => {
             let newImage = new Image(item);
             newImage.render();
-        })
-    })
+        });
+    });
     let newArray = [];
     Image.prototype.render = function () {
         if (!(newArray.includes(this.keyword))) {
@@ -25,27 +24,25 @@ $(function () {
             <option>${this.keyword}</option>
         `);
         }
-        let imageCopy = $('#photo-template').first().clone();
+        let imageCopy = $('.photo-class').clone();
+        imageCopy.removeClass('photo-class');
         imageCopy.find('h2').text(this.title);
         imageCopy.find('img').attr('src', this.image_url);
         imageCopy.find('p').text(this.description);
         $('main').append(imageCopy);
-    }
+    };
     $('select').on('change', function (event) {
         $('div').hide();
-        $('#firstMain').html('<div id="photo-template"><h2></h2><img src="" alt=""><p></p></div>');
+        $('#firstMain').html('<div class="photo-class" id="photo-template"><h2></h2><img src="" alt=""><p></p></div>');
         let selectValue = event.target.value;
         $.ajax('./data/page-1.json').then(data => {
-            // console.log(data);
             data.forEach((item) => {
                 if (item.keyword === selectValue) {
-                    console.log(item.keyword);
+                    console.log(item);
                     let newI = new Image(item);
                     newI.render();
-                    console.log(newI);
-                    // console.log('zaid')
                 }
-            })
-        })
-    })
-})
+            });
+        });
+    });
+});
